7dd17e847aa643477c84329587dc2b04f60675ba
hofmannsven <info@hofmannsven.com>
2014-02-07 12:17:55 +0100

    Live preview for markers to add

--- admin/assets/js/markposts.js
+++ admin/assets/js/markposts.js
@@ -13,4 +13,19 @@
 
 	});
 
+
+	// live preview of new markers
+	$('.js-add-markers').keyup(function(e) { // use keyup instead of keypress for latest char
+		var markers = [];
+		var make_markers = $(this).val().split(","); // separate marker by comma
+		$(make_markers).each(function(e) { // push each new marker to array
+			markers.push('<span class="new-marker">'+make_markers[e]+'</span>');
+		});
+
+		$('.js-new-markers-intro').show();
+		$('.js-new-markers').html(markers.join(' ')); // preview new markers
+
+	});
+
+
 }(jQuery));
--- admin/views/admin.php
+++ admin/views/admin.php
@@ -21,9 +21,9 @@ add_option( 'default_mark_posts_posttypes', $default_marker_post_types );
 
 // save form data
 function validate_form() {
-	
+
 	if ( $_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit']) ) {
-		
+
 		// just for debugging
 		// print_r($_POST);
 
@@ -32,11 +32,11 @@ function validate_form() {
 			$markertypes = $_POST['markertypes'];
 		else
 			$markertypes = array();
-			
+
 		$get_mark_posts_settings = get_option( 'mark_posts_settings' );
 		$set_mark_posts_settings = $markertypes;
 		$get_mark_posts_settings['mark_posts_posttypes'] = $set_mark_posts_settings;
-	
+
 		update_option( 'mark_posts_settings', $get_mark_posts_settings );
 
 		// update marker terms
@@ -45,7 +45,7 @@ function validate_form() {
 			$marker = trim($marker);
 			wp_insert_term( $marker, 'marker' );
 		}
-		
+
 		// update markers
 		$i=0;
 		if(ISSET($_POST['markernames'])) {
@@ -58,21 +58,21 @@ function validate_form() {
 				$i++;
 			}
 		}
-		
+
 		// delete markers
 		if(ISSET($_POST['delete'])) {
 			foreach($_POST['delete'] as $term_id) {
 				wp_delete_term( $term_id, 'marker' );
 			}
 		}
-		
+
 		echo display_settings_updated();
-	    
+
 	}
 }
 
 function display_settings_updated() {
-	
+
 	return '<div id="message" class="updated">
 		<p>'._x('Settings saved.', 'mark-posts').'</p>
 		</div>';
@@ -101,7 +101,7 @@ function get_all_types() {
 }
 
 function show_settings() {
-	
+
 	// set default colors
 	$default_colors = array('#96D754', '#FFFA74', '#FF7150', '#9ABADC', '#FFA74C', '#158A61');
 
@@ -123,10 +123,10 @@ function show_settings() {
 		echo '<h3 class="title">' . __('Marker Categories', 'mark-posts') . '</h3>';
 
 		echo '<table class="form-table"><tbody>';
-		
+
 		$i=0;
 		foreach($markers_terms as $marker_term) {
-			
+
 			if($marker_term->description != '')
 				$color = $marker_term->description;
 			else {
@@ -138,7 +138,7 @@ function show_settings() {
 					$color = $default_colors[$i];
 				}
 			}
-			
+
 			echo '<tr valign="top"><th scope="row"><input type="text" name="markernames[]" value="'.$marker_term->name.'"></th>';
 			echo '<td width="130"><input type="text" name="colors[]" value="'.$color.'" class="my-color-field" data-default-color="'.$color.'"/></td>';
 			echo '<td><input type="checkbox" name="delete[]" id="delete_'.$marker_term->term_id.'" value="'.$marker_term->term_id.'"> <label for="delete_'.$marker_term->term_id.'">'. __('delete', 'mark-posts') .'?</label> </td>';
@@ -147,22 +147,27 @@ function show_settings() {
 		}
 
 		echo '</tbody></table>';
-	
+
 		submit_button();
-		
+
 		echo '<hr />';
 	}
 
 	?>
 
-		
+
 		<h3 class="title"><?php _e('Add new Marker Categories', 'mark-posts'); ?></h3>
 		<p>
 			<?php _e('Add new marker types - for example (please separate them by comma):', 'mark-posts'); ?><br />
 			<strong><em><?php _e('Ready to go, Not quite finished, Not finished yet', 'mark-posts'); ?></em></strong>
 		</p>
 
-		<textarea name="markers" style="width:60%;height:120px;"></textarea>
+		<textarea class="js-add-markers" name="markers" style="width:60%;height:120px;"></textarea>
+		<div class="new-markers">
+			<span class="js-new-markers-intro"><?php _e('Markers to add:', 'mark-posts'); ?></span> <span class="js-new-markers"></span>
+		</div>
+
+		<?php submit_button(); ?>
 
 		<hr />
 		<h3 class="title"><?php _e('Enable/Disable Marker', 'mark-posts'); ?></h3>
@@ -184,7 +189,7 @@ function show_settings() {
 <div class="wrap">
 
 	<?php screen_icon(); ?>
-	
+
 	<?php validate_form(); ?>
 
 	<h2><?php _e('Mark Posts Options', 'mark-posts'); ?></h2>
