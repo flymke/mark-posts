4a4b5201a32fbc232b1fe38f3115fa3b7dd43d7b
hofmannsven <info@hofmannsven.com>
2014-05-21 23:15:20 +0200

    Add fallback for empty dashboard if everything is in trash

--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -16,7 +16,7 @@ if ( ! defined( 'WPINC' ) ) {
 
 
 /**
- * Get marker
+ * Get marker terms
  *
  * @since     1.0.0
  */
@@ -25,29 +25,33 @@ $marker_args = array(
 );
 $markers = get_terms( 'marker', $marker_args );
 
+/**
+ * Get all markers but trashed
+ *
+ * @since     1.0.7
+ */
+global $wpdb;
+$marker_items = '';
+if ( ! empty( $markers ) ) :
+	foreach ( $markers as $marker ) :
+		$trashed = $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->posts p JOIN $wpdb->term_relationships rl ON p.ID = rl.object_id WHERE rl.term_taxonomy_id = $marker->term_id AND p.post_status = 'trash' LIMIT 1" );
+		$count   = $marker->count - $trashed;
+		if ( $count > 0 ) :
+			$marker_items .= '<li class="mark-posts-info mark-posts-' . $marker->slug . '"><span>' . $count . ' ' . $marker->name . '</span></li>';
+		endif;
+	endforeach;
+endif;
+
 ?>
 
 <div class="main">
 	<ul id="markers_right_now">
 		<?php
-		if ( ! empty( $markers ) ) :
-			foreach ( $markers as $marker ) :
-				/**
-				 * Get trashed marker and remove them from total count
-				 *
-				 * @since     1.0.7
-				 */
-				global $wpdb;
-				$trashed= $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->posts p JOIN $wpdb->term_relationships rl ON p.ID = rl.object_id WHERE rl.term_taxonomy_id = $marker->term_id AND p.post_status = 'trash' LIMIT 1");
-				$count = $marker->count - $trashed;
-				if ( $count > 0 ) :
-					echo '<li class="mark-posts-info mark-posts-' . $marker->slug . '"><span>' . $count . ' ' . $marker->name . '</span></li>';
-				endif;
-			endforeach;
-		else:
+		if ( ! empty( $marker_items ) ) :
+			echo $marker_items;
+		else :
 			_e( 'No marked posts yet.', 'mark-posts' );
 		endif;
 		?>
-	</ul>
-	<!-- /#markers_right_now -->
+	</ul><!-- /#markers_right_now -->
 </div><!-- /.main -->
